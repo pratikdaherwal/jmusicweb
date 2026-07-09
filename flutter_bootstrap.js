16b170b739fd415e99d4e419eb325698a9627043
Pratik Daharwal <daherwalprince@gmail.com>
2026-07-09 10:24:08 +0530

    Build web with updated base-href

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2654482033" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1006521173" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
